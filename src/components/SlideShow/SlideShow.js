05c4de41067f5225f98a70be65c2c095dd4644c6
Jeff <jcameronjeff@gmail.com>
2019-02-08 11:29:41 -0500

    init

---
+++ src/components/SlideShow/SlideShow.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./SlideShow.scss";
+
+export default class SlideShow extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Slides Here</h1>
+      </div>
+    );
+  }
+}
